38d8bddfdc51f18862b66dd7f8e2740114274673
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-05 17:42:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,11 @@
 class School
  def initialize(name)
    @name = name
-   @roster = roster{}
+   @roster = {}
  end
 attr_accessor :roster
 
  def add_student(student_name, grade)
-   
+   @roster[student_name]
  end
  end
