33be686a6fc9616f1f3c0bedc760e1bcc6a225cf
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-05 18:13:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  def initialize(name)
+   @name = name
+   @roster = {}
+  end
+  attr_accessor :roster
+
+  def add_student(student_name, grade)
+    if roster.include?(grade)
+     roster[grade] << student_name
+    else
+     roster[grade] = []
+     roster[grade]=[student_name]
+    end
+  end
+  def grade(grade)
+    @grade = grade
+    @grade = roster[grade]
+  end
+  def sort
+    sort_hash = {}
+    roster.each do |x,y|
+      sort_hash [x] = y.sort
+    end
+    sort_hash
+  end
+ end
