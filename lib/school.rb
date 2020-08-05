4dd0ee3b63a6174a43c3acc3f563af24342c2363
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-05 17:53:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,6 @@ attr_accessor :roster
  def add_student(student_name, grade)
    @roster[grade]=[student_name]
    @roster[grade]= []
-   @roster[grade] << [student_name]
+   @roster[grade] << student_name
  end
  end
